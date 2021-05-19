6a676471d34f72cd5d593f3092f79d1a9a3c7d9c
Catalin-Lucian <catalin-lucian.cirstea@student.tuiasi.ro>
2021-05-19 12:48:18 +0300

    ceva aici

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace LogicalSchemeInterpretor.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap backiee_113541 {
+            get {
+                object obj = ResourceManager.GetObject("backiee-113541", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Memariani {
             get {
                 object obj = ResourceManager.GetObject("Memariani", resourceCulture);
